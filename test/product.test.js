0c8863511c0b9a9d751647cf92556b9937efdd92
Gautam Rangan <gautam1999rangan@gmail.com>
2023-11-18 10:33:37 +0530

    test file change

--- test/product.test.js
+++ test/product.test.js
@@ -8,10 +8,10 @@ describe('Testing POST Request /createproduct endpoint', () => {
     const response = await request(app)
       .post('/createproduct')
       .send({
-        title:"Mens Cotton Jacket",
+        title:"Women Cotton Jacket",
         price:55.99,
         description:"great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-        category:"men's clothing",
+        category:"Women's clothing",
         image:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
     });
 
